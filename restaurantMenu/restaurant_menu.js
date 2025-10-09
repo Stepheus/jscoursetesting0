2b67985a4197977def4f7e8bb7480d0cfc31e1e7
Stepheus <stephen.iboudah@outlook.com>
2025-10-09 10:24:27 -0400

    restaurant folder created

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,5 @@
+const breakfastMenu = ["Pancakes","Egss Benedist","Oatmeal", "Frittata"];
+const mainCourseMenu = ["Steak", "Pasta", "Burger", "Salmon"];
+const dessertMenu = ["Cake","Ice Cream","Pudding", "Fruit Salad"];
+
+
