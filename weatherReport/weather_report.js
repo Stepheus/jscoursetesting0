30e3d5eba45d73ebe7dd2b41a705f70c2577aeb0
Stepheus <stephen.iboudah@outlook.com>
2025-10-28 16:40:21 -0400

    Weather ajax completed fetch api

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,16 @@
+function showweatherDetails(event) {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const apiKey = "163837bddc02b030c2b8fdc6794d884d"; // Replaced 'API_KEY'
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                  <p>Temperature: ${data.main.temp} &#8451;</p>
+                                  <p>Weather: ${data.weather[0].description}</p>`;
+        })
+}
+
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
